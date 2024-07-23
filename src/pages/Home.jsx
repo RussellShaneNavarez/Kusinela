69e17d7341cb9e336dcc6ee2d872b3b9aa1c62b3
Russell Shane Navarez <russellnavarez18@gmail.com>
2024-07-23 16:36:01 +0200

    EDIT: Home.jsx responsive completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,8 +6,8 @@ const Home = () => {
   return (
     <div className="bg-cover bg-center bg-no-repeat h-screen" style={{ backgroundImage: `url(${backgroundImage})` }}>
       <div className="flex flex-col items-center justify-center h-full bg-black bg-opacity-50">
-        <img src={LogoName} alt="Logo" className='w-[40rem]' />
-        <h1 className="text-white text-3xl text-center w-[40rem]">A restaurant that gives you an experience of some taste of Filipino cuisine</h1>
+        <img src={LogoName} alt="Logo" className='w-[20rem] sm:w-[30rem] md:w-[40rem]' />
+        <h1 className="text-white text-xl sm:text-2xl md:text-3xl text-center md:w-[40rem] px-4">A restaurant that gives you an experience of some taste of Filipino cuisine</h1>
       </div>
     </div>
   );
