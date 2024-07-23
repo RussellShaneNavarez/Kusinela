2bd306f7f7c4bae91a6fb94fafd37b1d34efd612
Russell Shane Navarez <russellnavarez18@gmail.com>
2024-07-23 15:52:09 +0200

    EDIT: Order.jsx respnsive completed

--- src/pages/Order.jsx
+++ src/pages/Order.jsx
@@ -13,15 +13,15 @@ const Order = () => {
   return (
     <div className="flex flex-col bg-[#090833] text-white pt-20 pb-20 justify-center items-center">
 
-      <div className='flex flex-col items-center gap-2'>
-        <h2 className='text-3xl font-semibold'>You can't come and still want to eat?</h2>
+      <div className='flex flex-col justify-center items-center text-center gap-2 mx-12'>
+        <h2 className='md:text-3xl text-3xl font-semibold'>You can't come and still want to eat?</h2>
         <p>You can now order our food online! We are available at:</p>
       </div>
 
-      <div className='flex mt-20 mb-8 gap-16'>
+      <div className='flex mt-20 mb-8 gap-[8rem] flex-col md:flex-row sm:flex-row'>
 
-      <div className='flex flex-col gap-4'>
-        <img src={glovo} alt="Glovo" style={{width: 200, height: 200}} />
+      <div className='flex flex-col gap-4 h-[10rem] w-[10rem]'>
+        <img src={glovo} alt="Glovo" width="100%" height="100%"/>
         <button
           onClick={handleButtonClickGlovo}
           className='pt-2 pb-2 px-4 py-4 bg-[#FE1242] rounded-lg hover:bg-[#fe1241c0]'
@@ -30,8 +30,8 @@ const Order = () => {
         </button>
       </div>
 
-      <div className='flex flex-col gap-4'>
-        <img src={deliveroo} alt="Deliveroo" style={{width: 200, height: 200}}   />
+      <div className='flex flex-col gap-4 h-[10rem] w-[10rem]'>
+        <img src={deliveroo} alt="Deliveroo" width="100%" height="100%"  />
         <button
           onClick={handleButtonClickDeliveroo}
           className='pt-2 pb-2 px-4 py-4 bg-[#FE1242] rounded-lg hover:bg-[#fe1241c0]'
