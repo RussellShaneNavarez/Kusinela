e8b3b8cdf38fbac431ab5c4c32ace26dc638b18f
Russell Shane Navarez <russellnavarez18@gmail.com>
2024-07-26 15:15:24 +0200

    ADD: menu.pdf

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import menuImage from '../assets/menuImage.jpg';
 
 const Menu = () => {
-  const handleButtonClick = () => {
-    window.location.href = 'https://www.thefork.it/ristorante/kusinela-r800047/menu';
+  const menu = '/assets/menu.pdf';
+
+  const openPdf = () => {
+    window.open(menu, '_blank');
   };
+
   return (
     <div className='flex flex-col-reverse md:flex-row bg-[#090833] text-white text-center md:text-right '>
       <div className='flex mt-10 md:mt-20 mb-10 md:mb-20 md:w-1/2 sm:w-100 justify-center items-center'>
@@ -16,10 +19,10 @@ const Menu = () => {
         <h2 className='text-3xl mb-8 font-semibold'>
           Best <span style={{ color: '#FCD717' }}>Sellers</span>!
         </h2>
-        <p >Discover our best-sellers menu, featuring the most popular and beloved dishes crafted with the finest ingredients for an unforgettable dining experience</p>
-        <p >Indulge in customer favorites and chef-recommended delights that showcase our commitment to exceptional flavor and quality.</p>
+        <p>Discover our best-sellers menu, featuring the most popular and beloved dishes crafted with the finest ingredients for an unforgettable dining experience</p>
+        <p>Indulge in customer favorites and chef-recommended delights that showcase our commitment to exceptional flavor and quality.</p>
         <div className='mt-4'>
-          <button onClick={handleButtonClick} className='pt-2 pb-2 px-8 py-8 bg-[#FE1242] rounded-lg hover:bg-[#fe1241c0]'>Menù</button>
+          <button onClick={openPdf} type="button" className='pt-2 pb-2 px-8 py-8 bg-[#FE1242] rounded-lg hover:bg-[#fe1241c0]'>Menù</button>
         </div>
       </div>
     </div>
